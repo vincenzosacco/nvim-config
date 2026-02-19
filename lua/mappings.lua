151a16ea929cca0b61a6eb6c221d2cb341527470
saccolinux <93777997+vincenzosacco@users.noreply.github.com>
2026-02-19 17:14:43 +0100

    UPDATE mappings -> remove unecessary code

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,31 +10,11 @@ setMap({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 setMap({ "n", "i", "v" }, "<C-a>", "gg0vG$")
 
 --Make
-local function get_best_makeprg()
-  -- 1. Definiamo i nomi dei file che vogliamo cercare
-  local win_makefile = "windows.mk"
-  local linux_makefile = "linux.mk"
-
-  if is_windows then
-    vim.opt.shellpipe = ">%s 2>&1"
-    -- Try Windows-specific Makefile first
-    if vim.fn.filereadable(win_makefile) == 1 then
-      return "mingw32-make -f " .. win_makefile
-    else
-      return "mingw32-make" -- default mingw32-make search for "GNUmakefile", "makefile" or "Makefile"
-    end
-  else -- Linux
-    -- Try Linux-specific Makefile first
-    if vim.fn.filereadable(linux_makefile) == 1 then
-      return "make -f " .. linux_makefile
-    else
-      return "make" -- default make search for "GNUmakefile", "makefile" or "Makefile"
-    end
-  end
+if is_windows then
+  vim.opt.shellpipe = ">%s 2>&1"
+  -- Try Windows-specific Makefile first
+  vim.opt.makeprg = "mingw32-make" -- default mingw32-make search for "GNUmakefile", "makefile" or "Makefile"
 end
-
--- Applichiamo la configurazione
-vim.opt.makeprg = get_best_makeprg()
 
 -- 3. Map <leader>m to the native :make command
 setMap("n", "m", ":make<cr>", { desc = "run makefile" })
